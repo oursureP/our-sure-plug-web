9c88b6f64b13b4191924dde0d41b5d2e93be8664
Nkwocha Nicholas <nicholaslechi41@gmail.com>
2026-07-23 11:58:38 +0100

    fixed interface error

--- app/interfaces/index.ts
+++ app/interfaces/index.ts
@@ -2,4 +2,8 @@ export * from "./enums";
 export * from "./user.interface";
 export * from "./lms.interface";
 export * from "./blog.interface";
+// export * from "./marketing.interface";
+export * from "./lms.interface";
+export * from "./blog.interface";
 export * from "./marketing.interface";
+export type { Testimonial } from "./lms.interface";
